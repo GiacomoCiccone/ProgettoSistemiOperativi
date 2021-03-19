8f7077cebc61f8a1bf8c1f94dc5ec8f09cecf949
Agente Drif <giacomo98ciccone@gmail.com>
2021-03-20 00:54:22 +0100

    inizializzate alcune variabili globali

--- src/phase2/main.c
+++ src/phase2/main.c
@@ -2,14 +2,34 @@
 #include "pandos_types.h"
 #include "pcb.h"
 #include "asl.h"
+#include "p2test.c"
 
-int p_count;       //process count
-int sb_count;      //soft-block count
-pcb_PTR ready_q;   //ready queue
-pcb_PTR curr_proc; //current process
-//TODO Device semaphores
+#define SEM_NUM 49  //numero di semafori da mantenere
+
+int p_count;          //process count
+int sb_count;         //soft-block count
+pcb_PTR ready_q;      //ready queue
+pcb_PTR curr_proc;    //current process
+int dev_sem[SEM_NUM]; //device demaphores
 
 
 int main(){
-    
-}
+
+
+    /*pass up vector qui*/
+
+
+    initPcbs();  //inizializza la pcb_free
+    initASL();   //inizializza la ASL list
+
+    /*init variabili globali*/
+    p_count = 0;
+    sb_count = 0;
+    ready_q = mkEmptyProcQ();
+    curr_proc = NULL;
+    for(unsigned int i = 0; i < SEM_NUM; i++)
+    {
+        dev_sem[i] = 0;
+    }
+
+      
