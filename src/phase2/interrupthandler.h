f891037f8801d9ed782bf04dcb2a2b034d7b60c5
Mauro Molari <molarimauro00@gmail.com>
2021-03-23 11:14:36 -0400

    sistemati import

--- src/phase2/interrupthandler.h
+++ src/phase2/interrupthandler.h
@@ -1,9 +1,14 @@
+#include "helper.h"
 #include "pandos_types.h"
 #include "pandos_const.h"
-#include "main.h"
 #include "pcb.h"
 #include "asl.h"
 #include "syscall.h"
+
+#ifndef MAIN_H_
+#define MAIN_H_
+#include "main.h"
+#endif
 
 #define INTERRUPTLINEBASEADDR 0x1000002C;
 #define INTERRUPTINGLINEBASEADDR 0x10000040;
