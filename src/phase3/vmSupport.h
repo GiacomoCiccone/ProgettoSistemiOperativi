92b3c668a41efdd692a51d66ba374beead10749a
Agente <giacomo98ciccone@gmail.com>
2021-06-26 18:20:46 +0200

    .

--- src/phase3/vmSupport.h
+++ src/phase3/vmSupport.h
@@ -2,18 +2,16 @@
 #ifndef VM_SUPPORT_H
 #define VM_SUPPORT_H
 
-#include "../pandos_const.h"
-#include "../h/types.h"
-#include "../phase2/main.h"
-#include "initProc.h"
 
 
 void kill();
 void initTLB();
-void toggleInts(int status);
 void uTLB_RefillHandler();
 void pager();
-void programTrap();
+int chooseVictim();
+int flashCommand(int command, int victimLocation, int poolID,int flashDevNum);
+void clearSwap(int asid);
+void updateTLB(int page);
 
 
 #endif
